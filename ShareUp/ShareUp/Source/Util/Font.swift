60f77bf5fda4c605dc86bac5eabb515008d5acc8
Ga-yo <39812@naver.com>
2021-03-24 15:17:21 +0900

    [Feat] Font structure

---
+++ ShareUp/ShareUp/Source/Util/Font.swift
@@ -0,0 +1,17 @@
+//
+//  Font.swift
+//  ShareUp
+//
+//  Created by 이가영 on 2021/03/24.
+//
+
+import UIKit
+
+enum Font: String {
+    case interM = "Inter-Medium"
+    case interR = "Inter-Regular"
+    case interSB = "Inter-SemiBold"
+    case nsB = "NotoSansKR-Bold"
+    case nsM = "NotoSansKR-Medium"
+    case nsR = "NotoSansKR-Regualr"
+}
