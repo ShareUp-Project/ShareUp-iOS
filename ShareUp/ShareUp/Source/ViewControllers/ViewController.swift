eee450a12293d995a19f56929eebe5afa0031212
Ga-yo <39812@naver.com>
2021-03-26 11:57:28 +0900

    [Add] security on/off button

--- ShareUp/ShareUp/Source/ViewControllers/ViewController.swift
+++ ShareUp/ShareUp/Source/ViewControllers/ViewController.swift
@@ -18,13 +18,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var forgotPasswordButton: UIButton!
     @IBOutlet weak var signInButton: UIButton!
     @IBOutlet weak var autoAuthButton: UIButton!
+    @IBOutlet weak var securityOnOffButton: UIButton!
     
     private let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        securityOnOffButton.rx.tap.asDriver{ _ in .never() }.drive(onNext: { [weak self] in self?.updateCurrentStatus() }).disposed(by: disposeBag)
     }
-
+    
+    private func updateCurrentStatus() {
+        passwordTextField.isSecureTextEntry.toggle()
+        securityOnOffButton.setTitle(passwordTextField.isSecureTextEntry ? "숨기기" : "보기", for: .normal)
+    }
 }
 
