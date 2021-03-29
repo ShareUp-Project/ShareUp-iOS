a7e4a26da9e689e1653172e4f2fc87bb85b61af2
Ga-yo <39812@naver.com>
2021-03-29 11:53:20 +0900

    [Feat] AuthService

---
+++ ShareUp/ShareUp/Source/Service/AuthService.swift
@@ -0,0 +1,64 @@
+//
+//  AuthAPI.swift
+//  ShareUp
+//
+//  Created by 이가영 on 2021/03/29.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+import Moya
+
+class AuthService {
+    let provider = MoyaProvider<ShareUpAPI>()
+    
+    func signIn(_ phone: String,_ password: String) -> Observable<(StatusRules)> {
+        return provider.rx.request(.signIn(phone, password))
+            .filterSuccessfulStatusCodes()
+            .asObservable()
+            .map(Tokens.self)
+            .map { token -> (StatusRules) in
+//                if StoregaeManager.shared.create(token) { return (.ok) }
+                return (.fail)
+            }.catchError { [unowned self] in return .just(setNetworkError($0)) }
+    }
+    
+    func signUp(_ phone: String, nickname: String, password: String) -> Observable<(StatusRules)> {
+        return provider.rx.request(.signUp(phone, nickname, password))
+            .filterSuccessfulStatusCodes()
+            .asObservable()
+            .map { _ -> StatusRules in return (.ok) }
+            .catchError { [unowned self] in return .just(setNetworkError($0))}
+    }
+    
+    func phoneCertify(_ phone: String) -> Observable<(StatusRules)> {
+        return provider.rx.request(.phoneCertify(phone))
+            .filterSuccessfulStatusCodes()
+            .asObservable()
+            .map { _ -> StatusRules in return (.ok)}
+            .catchError { [unowned self] in return .just(setNetworkError($0))}
+    }
+    
+    func nicknameCheck(_ nickname: String) -> Observable<(StatusRules)> {
+        return provider.rx.request(.nicknameCheck(nickname))
+            .filterSuccessfulStatusCodes()
+            .asObservable()
+            .map { _ -> StatusRules in return (.ok)}
+            .catchError { [unowned self] in return .just(setNetworkError($0))}
+    }
+    
+    func passwordReset(_ phone: String, _ password: String) -> Observable<(StatusRules)> {
+        return provider.rx.request(.passwordReset(phone, password))
+            .filterSuccessfulStatusCodes()
+            .asObservable()
+            .map { _ -> StatusRules in return (.ok)}
+            .catchError { [unowned self] in return .just(setNetworkError($0))}
+    }
+    
+    func setNetworkError(_ error: Error) -> StatusRules {
+        guard let status = (error as? MoyaError)?.response?.statusCode else { return (.fail) }
+        return (StatusRules(rawValue: status) ?? .fail)
+    }
+    
+}
