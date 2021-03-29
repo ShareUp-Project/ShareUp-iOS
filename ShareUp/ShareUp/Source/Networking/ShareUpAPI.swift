c51ed8e5c2198e08dbbd1299b53e841e30ba96c5
Ga-yo <39812@naver.com>
2021-03-29 11:35:38 +0900

    [Add] Auth API

--- ShareUp/ShareUp/Source/Networking/ShareUpAPI.swift
+++ ShareUp/ShareUp/Source/Networking/ShareUpAPI.swift
@@ -6,3 +6,68 @@
 //
 
 import Foundation
+import Moya
+
+enum ShareUpAPI {
+    case signUp(_ phoneNum: String, _ nickname: String, _ password: String)
+    case phoneCertify(_ phoneNum: String)
+    case nicknameCheck(_ nickname: String)
+    case passwordReset(_ phoneNum: String, _ password: String)
+    case signIn(_ phoneNum: String, _ password: String)
+}
+
+extension ShareUpAPI: TargetType {
+    var baseURL: URL {
+        return URL(string: "")!
+    }
+    
+    var path: String {
+        switch self {
+        case .signUp:
+            return "/user"
+        case .phoneCertify:
+            return "/phone"
+        case .nicknameCheck:
+            return "/user/nickname"
+        case .passwordReset:
+            return "/user/password"
+        case .signIn:
+            return "/auth"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .nicknameCheck, .signIn, .signUp, .phoneCertify:
+            return .post
+        case .passwordReset:
+            return .put
+        }
+    }
+    
+    var sampleData: Data {
+        return Data()
+    }
+    
+    var task: Task {
+        switch self {
+        case .signIn(let phoneNum, let password):
+            return .requestParameters(parameters: ["phone": phoneNum, "password": password], encoding: JSONEncoding.prettyPrinted)
+        case .signUp(let phoneNum, let nickname, let password):
+            return .requestParameters(parameters: ["phone": phoneNum, "nickname": nickname, "password": password], encoding: JSONEncoding.prettyPrinted)
+        case .phoneCertify(let phoneNum):
+            return .requestParameters(parameters: ["phone": phoneNum], encoding: JSONEncoding.prettyPrinted)
+        case .nicknameCheck(let nickename):
+            return .requestParameters(parameters: ["nickname": nickename], encoding: JSONEncoding.prettyPrinted)
+        case .passwordReset(let phoneNum, let password):
+            return .requestParameters(parameters: ["password": password, "phone": phoneNum], encoding: JSONEncoding.prettyPrinted)
+        }
+    }
+    
+    var headers: [String : String]? {
+        switch self {
+        default:
+            return nil
+        }
+    }
+}
