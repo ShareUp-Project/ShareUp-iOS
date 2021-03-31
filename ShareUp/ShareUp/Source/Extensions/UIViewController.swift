e8d43ed84b3ee638a11ac328ef8e6be918b8416c
Ga-yo <39812@naver.com>
2021-03-31 15:02:34 +0900

    [ADD] func pushViewController

--- ShareUp/ShareUp/Source/Extensions/UIViewController.swift
+++ ShareUp/ShareUp/Source/Extensions/UIViewController.swift
@@ -13,4 +13,9 @@ extension UIViewController {
         let minutes: Int = (timer / 60) % 60
         return String(format: "%0d:%02d", minutes, seconds)
     }
+    
+    func pushViewController(_ identifier: String) {
+        let vc = storyboard?.instantiateViewController(withIdentifier: identifier)
+        navigationController?.pushViewController(vc!, animated: true)
+    }
 }
