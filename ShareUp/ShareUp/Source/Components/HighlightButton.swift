10b6c95df846294f6f6720793cecd6d9280c0cbe
Ga-yo <39812@naver.com>
2021-03-31 11:37:22 +0900

    [ADD] button highlighted custom

--- ShareUp/ShareUp/Source/Components/HighlightButton.swift
+++ ShareUp/ShareUp/Source/Components/HighlightButton.swift
@@ -5,4 +5,13 @@
 //  Created by 이가영 on 2021/03/31.
 //
 
-import Foundation
+import UIKit
+
+class HighlightedButton: UIButton {
+
+    override var isHighlighted: Bool {
+        didSet {
+            backgroundColor = isHighlighted ? MainColor.secondaryGreen : MainColor.primaryGreen
+        }
+    }
+}
