6b4dafd55e379d906bbd441d2e5c4b6adb377a64
Ga-yo <39812@naver.com>
2021-04-01 16:08:28 +0900

    [Feat] NewAuthVC

---
+++ ShareUp/ShareUp/Source/ViewControllers/NewAuthViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NewAuthViewController.swift
+//  ShareUp
+//
+//  Created by 이가영 on 2021/04/01.
+//
+
+import UIKit
+
+class NewAuthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
