9a49ad0fd3f480a3cbbbc3ef18429eb5fab3ae26
Ga-yo <39812@naver.com>
2021-04-01 20:23:29 +0900

    [ADD] FindAuthVC (reset password)

---
+++ ShareUp/ShareUp/Source/ViewControllers/FindAuthViewController.swift
@@ -0,0 +1,71 @@
+//
+//  FindAuthViewController.swift
+//  ShareUp
+//
+//  Created by 이가영 on 2021/03/31.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class FindAuthViewController: UIViewController {
+
+    @IBOutlet weak var countCertifyLabel: UILabel!
+    @IBOutlet weak var certifyRequestButton: UIButton!
+    @IBOutlet weak var numberTextField: AuthTextField!
+    @IBOutlet weak var authNumberTextField: AuthTextField!
+    @IBOutlet weak var errorLabel: UILabel!
+    @IBOutlet weak var certifyButton: HighlightedButton!
+    
+    private let viewModel = CertifyViewModel()
+    private var disposeBag = DisposeBag()
+    private var countDown: Int = 180 {
+        willSet {
+            if countDown < 0 { countCertifyLabel.isHidden = true }
+            countCertifyLabel.text = checkTimer(countDown)
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        bindViewModel()
+        navigationBarColor(.white)
+        managerTrait()
+    }
+    
+    private func bindViewModel() {
+        let input = CertifyViewModel.Input(phoneNum: numberTextField.rx.text.orEmpty.asDriver(),
+                                           phoneRequest: certifyRequestButton.rx.tap.asDriver(),
+                                           phoneCertify: authNumberTextField.rx.text.orEmpty.asDriver(),
+                                           certifyButton: certifyButton.rx.tap.asDriver())
+        let output = viewModel.transform(input)
+        let timer = Timer.new(every: 1.second) {[unowned self] _ in countDown -= 1 }
+
+        output.wait.emit(onNext: { [unowned self] error in
+                            errorLabel.text = error
+                            errorLabel.isHidden = false},
+                         onCompleted: {[unowned self] in
+                            errorLabel.isHidden = true
+                            countCertifyLabel.isHidden = false
+                            timer.start()
+                         }).disposed(by: disposeBag)
+        
+        output.result.emit(onNext: { [unowned self] error in
+                            errorLabel.text = error
+                            errorLabel.isHidden.toggle()},
+                           onCompleted: {[unowned self] in
+                            let vc = storyboard?.instantiateViewController(withIdentifier: "newPassword") as! SignUpViewController
+                            vc.phoneNumber = numberTextField.text!
+                            navigationController?.pushViewController(vc, animated: true)
+                           }).disposed(by: disposeBag)
+    }
+    
+    private func managerTrait() {
+        numberTextField.rx.text.orEmpty.subscribe(onNext: {[unowned self] text in numberTextField.checkPhoneCount(text)}).disposed(by: disposeBag)
+        certifyRequestButton.rx.tap.subscribe(onNext: {[unowned self] _ in
+            certifyRequestButton.setTitle("재요청", for: .normal)
+        }).disposed(by: disposeBag)
+    }
+}
