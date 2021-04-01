260fd145bc6e78a6f28e54319f01003c821b2cce
Ga-yo <39812@naver.com>
2021-04-01 20:01:22 +0900

    [ADD} NewAuthVC 일치, 불일치 실시간 추적

--- ShareUp/ShareUp/Source/ViewControllers/NewAuthViewController.swift
+++ ShareUp/ShareUp/Source/ViewControllers/NewAuthViewController.swift
@@ -6,24 +6,48 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class NewAuthViewController: UIViewController {
 
+    @IBOutlet weak var newTextField: AuthTextField!
+    @IBOutlet weak var securityOnOffButton: UIButton!
+    @IBOutlet weak var checkTextField: AuthTextField!
+    @IBOutlet weak var equalsButton: UILabel!
+    @IBOutlet weak var resetButton: UIButton!
+    
+    private let viewModel = NewAuthViewModel()
+    private let disposeBag = DisposeBag()
+    var phoneNumber = String()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        managerTrait()
+        bindViewModel()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func bindViewModel() {
+        let input = NewAuthViewModel.Input(phoneNum: Driver.just(phoneNumber),
+                                           newPassword: newTextField.rx.text.orEmpty.asDriver(),
+                                           againPassword: checkTextField.rx.text.orEmpty.asDriver(),
+                                           resetTap: resetButton.rx.tap.asDriver())
+        let output = viewModel.transform(input)
+        
+        output.isEquals.drive{[unowned self] value in
+            equalsButton.text = value ? "일치" : "불일치"
+            equalsButton.textColor = value ? MainColor.primaryGreen : MainColor.red
+        }.disposed(by: disposeBag)
+        
     }
-    */
-
+    
+    private func managerTrait() {
+        securityOnOffButton.rx.tap.asDriver{ _ in .never() }.drive(onNext: { [weak self] in self?.updateCurrentStatus() }).disposed(by: disposeBag)
+    }
+    
+    private func updateCurrentStatus() {
+        newTextField.isSecureTextEntry.toggle()
+        securityOnOffButton.setTitle(newTextField.isSecureTextEntry ? "보기" : "숨기기", for: .normal)
+    }
 }
