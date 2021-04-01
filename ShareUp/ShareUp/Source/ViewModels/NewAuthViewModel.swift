82ea7c091393be50e5eafd52d98605766c8e2396
Ga-yo <39812@naver.com>
2021-04-01 22:13:35 +0900

    [ADD] reset password check equal

--- ShareUp/ShareUp/Source/ViewModels/NewAuthViewModel.swift
+++ ShareUp/ShareUp/Source/ViewModels/NewAuthViewModel.swift
@@ -24,6 +24,7 @@ class NewAuthViewModel: ViewModelType {
         let result: Signal<String>
         let check: Signal<String>
         let isEquals: Driver<Bool>
+        let errorIsHidden: Driver<Bool>
     }
     
     func transform(_ input: Input) -> Output {
@@ -31,23 +32,29 @@ class NewAuthViewModel: ViewModelType {
         let check = PublishSubject<String>()
         let result = PublishSubject<String>()
         let checkInfo = Driver.combineLatest(input.newPassword, input.againPassword)
-        let resetInfo = Driver.combineLatest(input.phoneNum, input.againPassword)
+        let resetInfo = Driver.combineLatest(input.phoneNum, input.newPassword, input.againPassword)
         let isEquals = checkInfo.map { !$0.1.isEmpty && !$0.0.isEmpty && $0.0 == $0.1 ? true : false }
+        let errorIsHidden = checkInfo.map { ShareUpFilter.checkPassword($0.1) }
+
         
-        input.resetTap.asObservable().withLatestFrom(resetInfo).subscribe(onNext: {[weak self] phone, password in
+        input.resetTap.asObservable().withLatestFrom(resetInfo).subscribe(onNext: {[weak self] phone, newPassword, password in
             guard let self = self else { return }
-            api.passwordReset(phone, password).subscribe(onNext: { response in
-                switch response {
-                case .ok:
-                    result.onCompleted()
-                case .notFound:
-                    result.onNext("전화번호에 해당하는 유저가 없습니다.")
-                default:
-                    result.onNext("오류로 비밀번호 초기화가 되지 않습니다.")
-                }
-            }).disposed(by: self.disposeBag)
+            if newPassword != password {
+                result.onNext("비밀번호가 일치하지 않습니다.") }
+            else {
+                api.passwordReset(phone, password).subscribe(onNext: { response in
+                    switch response {
+                    case .ok:
+                        result.onCompleted()
+                    case .notFound:
+                        result.onNext("전화번호에 해당하는 유저가 없습니다.")
+                    default:
+                        result.onNext("오류로 비밀번호 초기화가 되지 않습니다.")
+                    }
+                }).disposed(by: self.disposeBag)
+            }
         }).disposed(by: disposeBag)
         
-        return Output(result: result.asSignal(onErrorJustReturn: ""), check: check.asSignal(onErrorJustReturn: ""), isEquals: isEquals.asDriver())
+        return Output(result: result.asSignal(onErrorJustReturn: ""), check: check.asSignal(onErrorJustReturn: ""), isEquals: isEquals.asDriver(), errorIsHidden: errorIsHidden.asDriver())
     }
 }
