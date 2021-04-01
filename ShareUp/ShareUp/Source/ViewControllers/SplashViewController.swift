0d1a5211e90fe0278434995f6de06e6a270cbe59
Ga-yo <39812@naver.com>
2021-04-01 09:16:32 +0900

    [Feat] SplashVC
    navigation bar hidden

---
+++ ShareUp/ShareUp/Source/ViewControllers/SplashViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SplashViewController.swift
+//  ShareUp
+//
+//  Created by 이가영 on 2021/03/31.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        navigationBarHidden()
+    }
+    
+}
