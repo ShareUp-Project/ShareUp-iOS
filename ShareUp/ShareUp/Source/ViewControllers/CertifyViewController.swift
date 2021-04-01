49ec947e83d3b67e19be29a078d08b63fc693b98
Ga-yo <39812@naver.com>
2021-04-01 20:23:53 +0900

    [ADD] CertifyVC connect to VM

--- ShareUp/ShareUp/Source/ViewControllers/CertifyViewController.swift
+++ ShareUp/ShareUp/Source/ViewControllers/CertifyViewController.swift
@@ -17,36 +17,57 @@ class CertifyViewController: UIViewController {
     @IBOutlet weak var numberTextField: AuthTextField!
     @IBOutlet weak var authNumberTextField: AuthTextField!
     @IBOutlet weak var errorLabel: UILabel!
-    @IBOutlet weak var certifyButton: UIButton!
+    @IBOutlet weak var certifyButton: HighlightedButton!
     
+    private let viewModel = CertifyViewModel()
     private var disposeBag = DisposeBag()
     private var countDown: Int = 180 {
         willSet {
+            if countDown < 0 { countCertifyLabel.isHidden = true }
             countCertifyLabel.text = checkTimer(countDown)
         }
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        numberTextField.rx.text.orEmpty.subscribe(onNext: {[unowned self] text in numberTextField.checkPhoneCount(text)}).disposed(by: disposeBag)
-        certifyRequestButton.rx.tap.subscribe(onNext: {[unowned self ] _ in countCertifyLabel.isHidden = false}).disposed(by: disposeBag)
-        
-        let timer = Timer.new(every: 1.second) { [unowned self] _ in countDown -= 1 }
-        certifyRequestButton.rx.tap.subscribe(onNext: {[unowned self] _ in
-            if countDown < 0 { timer.invalidate() }
-            timer.start()
-        })
+        bindViewModel()
+        navigationBarColor(.white)
+        managerTrait()
     }
     
-    /*
-    // MARK: - Navigation
+    private func bindViewModel() {
+        let input = CertifyViewModel.Input(phoneNum: numberTextField.rx.text.orEmpty.asDriver(),
+                                           phoneRequest: certifyRequestButton.rx.tap.asDriver(),
+                                           phoneCertify: authNumberTextField.rx.text.orEmpty.asDriver(),
+                                           certifyButton: certifyButton.rx.tap.asDriver())
+        let output = viewModel.transform(input)
+        let timer = Timer.new(every: 1.second) {[unowned self] _ in countDown -= 1 }
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        output.wait.emit(onNext: { [unowned self] error in
+                            errorLabel.text = error
+                            errorLabel.isHidden = false},
+                         onCompleted: {[unowned self] in
+                            errorLabel.isHidden = true
+                            countCertifyLabel.isHidden = false
+                            timer.start()
+                         }).disposed(by: disposeBag)
+        
+        output.result.emit(onNext: { [unowned self] error in
+                            errorLabel.text = error
+                            errorLabel.isHidden.toggle()},
+                           onCompleted: {[unowned self] in
+                            let vc = storyboard?.instantiateViewController(withIdentifier: "signup") as! SignUpViewController
+                            vc.phoneNumber = numberTextField.text!
+                            navigationController?.pushViewController(vc, animated: true)
+                           }).disposed(by: disposeBag)
     }
-    */
+    
+    private func managerTrait() {
+        numberTextField.rx.text.orEmpty.subscribe(onNext: {[unowned self] text in numberTextField.checkPhoneCount(text)}).disposed(by: disposeBag)
+        certifyRequestButton.rx.tap.subscribe(onNext: {[unowned self] _ in
+            certifyRequestButton.setTitle("재요청", for: .normal)
+        }).disposed(by: disposeBag)
+    }
 
 }
