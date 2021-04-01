bab723d214adc30087aecbed40507b81c7d1a7c6
Ga-yo <39812@naver.com>
2021-04-01 20:01:59 +0900

    [ADD] NewAuthVM reset password

--- ShareUp/ShareUp/Source/ViewModels/NewAuthViewModel.swift
+++ ShareUp/ShareUp/Source/ViewModels/NewAuthViewModel.swift
@@ -6,3 +6,48 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+class NewAuthViewModel: ViewModelType {
+    
+    private let disposeBag = DisposeBag()
+    
+    struct Input {
+        let phoneNum: Driver<String>
+        let newPassword: Driver<String>
+        let againPassword: Driver<String>
+        let resetTap: Driver<Void>
+    }
+    
+    struct Output {
+        let result: Signal<String>
+        let check: Signal<String>
+        let isEquals: Driver<Bool>
+    }
+    
+    func transform(_ input: Input) -> Output {
+        let api = AuthService()
+        let check = PublishSubject<String>()
+        let result = PublishSubject<String>()
+        let checkInfo = Driver.combineLatest(input.newPassword, input.againPassword)
+        let resetInfo = Driver.combineLatest(input.phoneNum, input.againPassword)
+        let isEquals = checkInfo.map { !$0.1.isEmpty && !$0.0.isEmpty && $0.0 == $0.1 ? true : false }
+        
+        input.resetTap.asObservable().withLatestFrom(resetInfo).subscribe(onNext: {[weak self] phone, password in
+            guard let self = self else { return }
+            api.passwordReset(phone, password).subscribe(onNext: { response in
+                switch response {
+                case .ok:
+                    result.onCompleted()
+                case .notFound:
+                    result.onNext("전화번호에 해당하는 유저가 없습니다.")
+                default:
+                    result.onNext("오류로 비밀번호 초기화가 되지 않습니다.")
+                }
+            }).disposed(by: self.disposeBag)
+        }).disposed(by: disposeBag)
+        
+        return Output(result: result.asSignal(onErrorJustReturn: ""), check: check.asSignal(onErrorJustReturn: ""), isEquals: isEquals.asDriver())
+    }
+}
