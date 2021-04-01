eea0dcc477a2585bb2cd9ceb6c6e7bc8b960195a
Ga-yo <39812@naver.com>
2021-04-01 14:33:23 +0900

    [ADD] TokenManager

--- ShareUp/ShareUp/Source/Manager/TokenManager.swift
+++ ShareUp/ShareUp/Source/Manager/TokenManager.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+struct TokenManager {
+    enum TokenSataus {
+        case access
+        case refresh
+    }
+    
+    static var currentToken: Tokens? {
+        return StoregaeManager.shared.read()
+    }
+    
+    static func accessTokenisUseful() -> Bool {
+        if (currentToken?.accessToken) != nil {
+            return true
+        }else {
+            return false
+        }
+    }
+    
+    static func refreshTokenisUseful() -> Bool {
+        if (currentToken?.refreshToken) != nil {
+            return true
+        }else {
+            return false
+        }
+    }
+}
