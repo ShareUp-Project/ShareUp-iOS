d7739fcb3291a34228dc8d9d3141f1fef3291092
Ga-yo <39812@naver.com>
2021-04-02 17:00:22 +0900

    [Feat] Token Model

---
+++ ShareUp/ShareUp/Source/Models/Tokens.swift
@@ -0,0 +1,13 @@
+//
+//  Tokens.swift
+//  ShareUp
+//
+//  Created by 이가영 on 2021/03/29.
+//
+
+import Foundation
+
+struct Tokens: Codable {
+    let accessToken: String
+    let refreshToken: String
+}
