968a7642b71a7de4efbea27dca8a99941db46d8a
Ga-yo <39812@naver.com>
2021-04-08 11:35:59 +0900

    [ADD] Posts Model

--- ShareUp/ShareUp/Source/Models/Posts.swift
+++ ShareUp/ShareUp/Source/Models/Posts.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+struct Posts: Codable {
+    let data: [Post]
+}
+struct Post: Codable {
+    let id: String
+    let title: String
+    let category: String
+    let views: Int
+    let user: User
+    let hashtags: [String]
+    let image: [Int]
+    let scraps: Int
+    let isScrap: Bool
+}
+
+struct User: Codable {
+    let id: String
+    let nickname: String
+}
