c2a54c4296f22b86c76eb5d3c87f8aca3e91914c
Ga-yo <39812@naver.com>
2021-04-09 14:11:34 +0900

    [ADD] getPosts in VM

--- ShareUp/ShareUp/Source/ViewModels/MainViewModel.swift
+++ ShareUp/ShareUp/Source/ViewModels/MainViewModel.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+class MainViewModel: ViewModelType {
+    private let disposeBag = DisposeBag()
+    
+    struct Input{
+        let getPosts: Completable
+    }
+    
+    struct Output{
+        let getPosts: Driver<[Post]>
+    }
+    
+    func transform(_ input: Input) -> Output {
+        let api = AuthService()
+        let result = PublishSubject<String>()
+        let getPostsData = PublishRelay<[Post]>()
+        
+        input.getPosts.asObservable().subscribe(onNext: { [weak self] _ in
+            guard let self = self else {return}
+            api.getPosts(0).asObservable().subscribe(onNext: { data, response in
+                switch response {
+                case .ok:
+                    getPostsData.accept(data!.data)
+                default:
+                    result.onNext("서버 오류")
+                }
+            }).disposed(by: self.disposeBag)
+        }).disposed(by: disposeBag)
+        
+        return Output(getPosts: getPostsData.asDriver(onErrorJustReturn: []))
+    }
+}
