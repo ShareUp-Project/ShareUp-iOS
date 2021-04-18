8d991adf75023dfdf909faa3954a7358a99bcd51
Ga-yo <39812@naver.com>
2021-04-18 21:57:40 +0900

    [ADD] page control with swipe gesture

--- ShareUp/ShareUp/Source/ViewControllers/DetailViewController.swift
+++ ShareUp/ShareUp/Source/ViewControllers/DetailViewController.swift
@@ -61,11 +61,43 @@ class DetailViewController: UIViewController {
             contentTextView.text = data.content
             viewsLabel.text = String(data.views)
             scrapsLabel.text = String(data.scraps)
+            if data.isMine {
+                navigationItem.rightBarButtonItem = deletePostButton
+            }
         }.disposed(by: disposeBag)
         
         output.scrapResult.asObservable().subscribe(onNext: {[unowned self] _ in
             loadData.accept(())
         }).disposed(by: disposeBag)
+        
+        output.result.emit(onCompleted : {
+            self.navigationController?.popViewController(animated: true)
+        }).disposed(by: disposeBag)
     }
     
+    private func setPagingGesture() {
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(respondToSwipeGesture(_:)))
+        swipeLeft.direction = UISwipeGestureRecognizer.Direction.left
+        self.view.addGestureRecognizer(swipeLeft)
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(respondToSwipeGesture(_:)))
+        swipeRight.direction = UISwipeGestureRecognizer.Direction.right
+        self.view.addGestureRecognizer(swipeRight)
+    }
+    
+    @objc func respondToSwipeGesture(_ gesture: UIGestureRecognizer) {
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer{
+            switch swipeGesture.direction {
+            case UISwipeGestureRecognizer.Direction.left :
+                pageControl.currentPage += 1
+                shareImageView.kf.setImage(with: URL(string: "https://shareup-s3.s3.ap-northeast-2.amazonaws.com/\(image[pageControl.currentPage])")!)
+            case UISwipeGestureRecognizer.Direction.right :
+                pageControl.currentPage -= 1
+                shareImageView.kf.setImage(with: URL(string: "https://shareup-s3.s3.ap-northeast-2.amazonaws.com/\(image[pageControl.currentPage])")!)
+            default:
+                break
+            }
+        }
+    }
+
 }
