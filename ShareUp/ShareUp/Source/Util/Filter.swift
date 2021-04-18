097c7865fa8704e9be4fc12e020775c4628f77cf
Ga-yo <39812@naver.com>
2021-04-18 21:55:54 +0900

    [ADD] category filter 추가

--- ShareUp/ShareUp/Source/Util/Filter.swift
+++ ShareUp/ShareUp/Source/Util/Filter.swift
@@ -11,4 +11,18 @@ struct ShareUpFilter {
     static func checkPassword(_ pw: String) -> Bool {
         return pw.range(of: "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$", options: .regularExpression) != nil
     }
+    
+    static func filterCategory(_ category: String) -> String {
+        switch category {
+        case "유리" : return "glass"
+        case "플라스틱" : return "plastic"
+        case "종이" : return "paper"
+        case "스티로폼" : return "styroform"
+        case "캔" : return "can"
+        case "비닐" : return "vinyl"
+        case "의류" : return "clothing"
+        case "기타" : return "etc"
+        default: return ""
+        }
+    }
 }
