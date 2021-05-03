492aa5cf8c5d1eb9911efa0703450b3bf739c4e2
Ga-yo <39812@naver.com>
2021-05-03 16:57:42 +0900

    [ADD] 카테고리 검색 및 디테일 보기

--- ShareUp/ShareUp/Source/ViewModels/SearchViewModel.swift
+++ ShareUp/ShareUp/Source/ViewModels/SearchViewModel.swift
@@ -14,24 +14,31 @@ final class SearchViewModel: ViewModelType {
 
     struct Input {
         let searchTap: Driver<Void>
+        let loadDetail: Driver<IndexPath>
         let searchContent: Driver<String?>
+        let searchCategory: Driver<String>
     }
 
     struct Output {
         let result: Signal<String>
         let getSearchPosts: Driver<[Post]>
         let isRecentlyOn: Driver<Bool>
+        let getCategoryPosts: Driver<[Post]>
+        let detailIndexPath: Signal<String>
     }
 
     func transform(_ input: Input) -> Output {
         let api = AuthService()
         let result = PublishSubject<String>()
         let getSearchData = BehaviorRelay<[Post]>(value: [])
-        let isRecentlyOn = input.searchContent.map { !($0?.isEmpty ?? false) }
+        let getCategoryPosts = BehaviorRelay<[Post]>(value: [])
+        let getDetailRow = PublishSubject<String>()
+        let isRecentlyOn = input.searchContent.map { $0?.isEmpty ?? false }
             
         input.searchTap.asObservable().withLatestFrom(input.searchContent).subscribe(onNext: { [weak self] word in
             guard let self = self else { return }
             api.searchPosts(word!, 0).subscribe(onNext: { data, response in
+                print(data)
                 switch response {
                 case .ok:
                     getSearchData.accept(data!.data)
@@ -41,6 +48,23 @@ final class SearchViewModel: ViewModelType {
             }).disposed(by: self.disposeBag)
         }).disposed(by: disposeBag)
         
-        return Output(result: result.asSignal(onErrorJustReturn: ""), getSearchPosts: getSearchData.asDriver(), isRecentlyOn: isRecentlyOn.asDriver())
+        input.searchCategory.asObservable()
+            .flatMap { category in api.getCategorySearch(0, category)}
+            .subscribe(onNext: { data, response in
+                print(response)
+                switch response {
+                case .ok:
+                    getCategoryPosts.accept(data!.data)
+                default:
+                    result.onNext("server error")
+                }
+            }).disposed(by: disposeBag)
+        
+        input.loadDetail.asObservable().subscribe(onNext: { indexPath in
+            let value = getCategoryPosts.value
+            getDetailRow.onNext(String(value[indexPath.row].id))
+        }).disposed(by: disposeBag)
+        
+        return Output(result: result.asSignal(onErrorJustReturn: ""), getSearchPosts: getSearchData.asDriver(), isRecentlyOn: isRecentlyOn.asDriver(), getCategoryPosts: getCategoryPosts.asDriver(onErrorJustReturn: []), detailIndexPath: getDetailRow.asSignal(onErrorJustReturn: ""))
     }
 }
