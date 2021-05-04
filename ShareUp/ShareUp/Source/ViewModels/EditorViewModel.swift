f2f97c842ed4152c61a064e6f3989c34174feacc
Ga-yo <39812@naver.com>
2021-05-04 10:11:19 +0900

    [ADD] EditorViewModel 추가

---
+++ ShareUp/ShareUp/Source/ViewModels/EditorViewModel.swift
@@ -0,0 +1,51 @@
+//
+//  EditorViewModel.swift
+//  ShareUp
+//
+//  Created by 이가영 on 2021/05/03.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+final class EditorViewModel: ViewModelType {
+    private let disposeBag = DisposeBag()
+    
+    struct Input{
+        let loadData: Driver<Void>
+        let loadDetail: Driver<IndexPath>
+    }
+    
+    struct Output{
+        let getEditorPosts: Driver<[EditorPost]>
+        let getDetailValue: Signal<EditorPost>
+    }
+    
+    func transform(_ input: Input) -> Output {
+        let api = AuthService()
+        let result = PublishSubject<String>()
+        let getEditorPosts = BehaviorRelay<[EditorPost]>(value: [])
+        let getDetailValue = PublishRelay<EditorPost>()
+        
+        input.loadData.asObservable()
+            .flatMap { _ in api.getEditorPosts(0)}
+            .subscribe(onNext: { data, response in
+                print(response)
+                switch response{
+                case .ok:
+                    getEditorPosts.accept(data!.data)
+                default:
+                    result.onNext("server error")
+                }
+            }).disposed(by: disposeBag)
+        
+        input.loadDetail.asObservable()
+            .subscribe(onNext: { indexPath in
+                let value = getEditorPosts.value
+                getDetailValue.accept(EditorPost(id: value[indexPath.row].id, title: value[indexPath.row].title, content: value[indexPath.row].content, image: value[indexPath.row].image))
+            }).disposed(by: disposeBag)
+        
+        return Output(getEditorPosts: getEditorPosts.asDriver(), getDetailValue: getDetailValue.asSignal())
+    }
+}
