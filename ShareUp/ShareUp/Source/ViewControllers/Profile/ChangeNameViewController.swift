3dc044383747f5917633c8547e90d679cc451111
Ga-yo <39812@naver.com>
2021-05-07 20:23:45 +0900

    [ADD] ChangeNicknameVC 추가

---
+++ ShareUp/ShareUp/Source/ViewControllers/Profile/ChangeNameViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ChangeNameViewController.swift
+//  ShareUp
+//
+//  Created by 이가영 on 2021/05/07.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class ChangeNameViewController: UIViewController {
+
+    @IBOutlet weak var nicknameTextField: AuthTextField!
+    @IBOutlet weak var changeButton: HighlightedButton!
+    @IBOutlet weak var duplicateLabel: UILabel!
+
+    private let disposeBag = DisposeBag()
+    private let viewModel = ChangeNameViewModel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        bindViewModel()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        tabBarController?.navigationController?.navigationItem.title = "닉네임 수정"
+        navigationBackCustom()
+    }
+    
+    private func bindViewModel() {
+        let input = ChangeNameViewModel.Input(nickname: nicknameTextField.rx.text.orEmpty.asDriver(),
+                                              doneTap: changeButton.rx.tap.asDriver())
+        let output = viewModel.transform(input)
+        
+        output.duplicateCheck.emit(onNext: { [unowned self] error in
+            duplicateLabel.isHidden = false
+            duplicateLabel.text = error
+        },onCompleted: { [unowned self] in
+            duplicateLabel.text = "가능"
+            duplicateLabel.textColor = MainColor.primaryGreen
+            output.result.emit(onCompleted: { [unowned self] in
+                navigationController?.popViewController(animated: true)
+            }).disposed(by: disposeBag)
+        }).disposed(by: disposeBag)
+    }
+    
+
+}
