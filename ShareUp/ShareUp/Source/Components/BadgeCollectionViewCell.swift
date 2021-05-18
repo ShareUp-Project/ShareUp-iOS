3ea5b2705af785887ef2a32787ecd0175b558b30
Ga-yo <39812@naver.com>
2021-05-18 16:20:04 +0900

    [ADD] BadgeCollectionViewCell 추가

---
+++ ShareUp/ShareUp/Source/Components/BadgeCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  BadgeCollectionViewCell.swift
+//  ShareUp
+//
+//  Created by 이가영 on 2021/05/18.
+//
+
+import UIKit
+
+final class BadgeCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var badgeImageView: UIImageView!
+    @IBOutlet weak var badgeNameLabel: UILabel!
+    
+}
+
