79b4caa848f443c9d4fbde9a58411cc68df0a568
Ga-yo <39812@naver.com>
2021-05-23 23:04:23 +0900

    [ADD] 등급별 배지 이미지 보여주기

--- ShareUp/ShareUp/Source/Components/BadgeCollectionViewCell.swift
+++ ShareUp/ShareUp/Source/Components/BadgeCollectionViewCell.swift
@@ -11,5 +11,26 @@ final class BadgeCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var badgeImageView: UIImageView!
     @IBOutlet weak var badgeNameLabel: UILabel!
     
+    func configCell(_ data: Int, indexPath: Int) {
+        switch indexPath {
+        case 0:
+            badgeImageView.image = UIImage(named: "first\(data)")
+        case 1:
+            badgeImageView.image = UIImage(named: "paper\(data)")
+        case 2:
+            badgeImageView.image = UIImage(named: "plastic\(data)")
+        case 3:
+            badgeImageView.image = UIImage(named: "glass\(data)")
+        case 4:
+            badgeImageView.image = UIImage(named: "styrofoam\(data)")
+        case 5:
+            badgeImageView.image = UIImage(named: "vinyl\(data)")
+        case 6:
+            badgeImageView.image = UIImage(named: "can\(data)")
+        case 7:
+            badgeImageView.image = UIImage(named: "clothing\(data)")
+        default:
+            print("no more badge")
+        }
+    }
 }
-
