48459c68588d8f1af71971386898d27e78fae903
Ga-yo <39812@naver.com>
2021-05-23 14:53:11 +0900

    [Feat] Badge model 추가

---
+++ ShareUp/ShareUp/Source/Models/Badge.swift
@@ -0,0 +1,19 @@
+//
+//  Badge.swift
+//  ShareUp
+//
+//  Created by 이가영 on 2021/05/23.
+//
+
+import Foundation
+
+struct Badge: Codable {
+    let first: Int
+    let paper: Int
+    let plastic: Int
+    let glass: Int
+    let styrofoam: Int
+    let vinyl: Int
+    let can: Int
+    let clothing: Int
+}
