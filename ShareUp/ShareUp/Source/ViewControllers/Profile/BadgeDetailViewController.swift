5caaea2c0885c399282965868b027e5fef024314
Ga-yo <39812@naver.com>
2021-05-27 22:03:27 +0900

    [ADD] 배지 등록하기 + 배지 설명 보여주기 추가

--- ShareUp/ShareUp/Source/ViewControllers/Profile/BadgeDetailViewController.swift
+++ ShareUp/ShareUp/Source/ViewControllers/Profile/BadgeDetailViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 import PanModal
 import CollectionViewPagingLayout
 
@@ -16,43 +18,94 @@ final class BadgeDetailViewController: UIViewController {
     @IBOutlet weak var badgeLabel: UILabel!
     @IBOutlet weak var badgeCountLabel: UILabel!
     @IBOutlet weak var badgeSetButton: UIButton!
-            
-    private let layout = CollectionViewPagingLayout()
-    private var badgeAcheive = [String]()
+    
     var category = String()
     var level = Int()
+    
+    private let layout = CollectionViewPagingLayout()
+    private var badgeAcheive = [String](repeating: "0", count: 3)
+    private let viewModel = BadgeDetailViewModel()
+    private let disposeBag = DisposeBag()
+    private var currentLevel = BehaviorRelay<Int>(value: 0)
+    private var currentName = PublishRelay<String>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        for i in 0..<level {
+            badgeAcheive[i] = (category + "\(i+1)")
+        }
+        
         layout.numberOfVisibleItems = 3
         badgeLevelCollectionView.collectionViewLayout = layout
         badgeLevelCollectionView.isPagingEnabled = true
-
+        badgeLevelCollectionView.indicatorStyle = .white
+        
+        
         layout.delegate = self
         badgeLevelCollectionView.dataSource = self
         badgeLevelCollectionView.delegate = self
-        
-        
-        for i in 1..<level {
-            badgeAcheive.append(category + "\(i)")
-        }
+        currentName.accept(category)
+        bindViewModel()
+        navigationBackCustom()
+    }
+    
+    private func bindViewModel() {
+        let input = BadgeDetailViewModel.Input(category: .just(category),
+                                               level: currentLevel.asDriver(),
+                                               postBadge: badgeSetButton.rx.tap.asDriver())
+        let output = viewModel.transform(input)
+        output.result.emit(onNext: { message in
+            print(message)
+        }, onCompleted: {
+            self.dismiss(animated: true, completion: nil)
+        }).disposed(by: disposeBag)
     }
 }
 
 extension BadgeDetailViewController: CollectionViewPagingLayoutDelegate, UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 3
+        if category == "default" || category == "first" {
+            return 1
+        }else {
+            return 3
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "badgeDetailCell", for: indexPath) as! BadgeLevelCollectionViewCell
-        cell.badgeImageView.image = UIImage(named: badgeAcheive[indexPath.row])
+        
+        if category == "default" || category == "first" {
+            cell.badgeImageView.image = UIImage(named: cell.badgeImage(category)[indexPath.row])
+            let info = Category(rawValue: category)?.toDescription()
+            badgeNameLabel.text = info?[0]
+            badgeLabel.text = info?[1]
+        }else {
+            cell.badgeImageView.image = UIImage(named: badgeAcheive[indexPath.row])
+        }
         return cell
     }
     
     func onCurrentPageChanged(layout: CollectionViewPagingLayout, currentPage: Int) {
-        
+        let info = Category(rawValue: badgeAcheive[currentPage])?.toDescription()
+        badgeNameLabel.text = info?[0]
+        badgeLabel.text = info?[1]
+        badgeCountLabel.text = info?[2]
+        currentLevel.accept(currentPage+1)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        if category == "default" || category == "first" {
+            let info = Category(rawValue: category)?.toDescription()
+            badgeNameLabel.text = info?[0]
+            badgeLabel.text = info?[1]
+        }else {
+            let info = Category(rawValue: badgeAcheive[0])?.toDescription()
+            badgeNameLabel.text = info?[0]
+            badgeLabel.text = info?[1]
+            badgeCountLabel.text = info?[2]
+            currentLevel.accept(1)
+        }
     }
     
 }
