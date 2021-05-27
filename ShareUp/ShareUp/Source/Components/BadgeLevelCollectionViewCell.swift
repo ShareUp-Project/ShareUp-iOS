3c4309a8f6063a7bd9e63c4c0106d12c38d8c21e
Ga-yo <39812@naver.com>
2021-05-27 22:07:09 +0900

    [ADD] BadgeLevelCollectionViewCell

--- ShareUp/ShareUp/Source/Components/BadgeLevelCollectionViewCell.swift
+++ ShareUp/ShareUp/Source/Components/BadgeLevelCollectionViewCell.swift
@@ -12,6 +12,24 @@ final class BadgeLevelCollectionViewCell: UICollectionViewCell, ScaleTransformVi
     
     @IBOutlet weak var badgeImageView: UIImageView!
 
-    var scaleOptions = ScaleTransformViewOptions(minScale: 0.4, scaleRatio: 0.4, translationRatio: CGPoint(x: 0.2, y: 0.2), maxTranslationRatio: CGPoint(x: 2, y: 0), keepVerticalSpacingEqual: true, scaleCurve: .linear, translationCurve: .linear, shadowEnabled: true, rotation3d: .none, translation3d: .none)
+    var scaleOptions = ScaleTransformViewOptions(minScale: 0.3, scaleRatio: 0.4, translationRatio: CGPoint(x: 0.2, y: 0.2), maxTranslationRatio: CGPoint(x: 2, y: 0), keepVerticalSpacingEqual: true, scaleCurve: .linear, translationCurve: .linear, shadowEnabled: false, rotation3d: .none, translation3d: .none)
     
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        badgeImageView.clipsToBounds = true
+        badgeImageView.layer.cornerRadius = 64
+    }
+    
+    func badgeImage(_ name: String) -> [String] {
+        switch name {
+        case "default":
+            return ["default0"]
+        case "first":
+            return ["first"]
+        default:
+            return [""]
+        }
+    }
 }
+
