a4f5433960a0edd721f127ab48fb4dd7df290b5c
Ga-yo <39812@naver.com>
2021-06-02 14:48:01 +0900

    [Feat] InfoVC

---
+++ ShareUp/ShareUp/Source/ViewControllers/Profile/InfoViewController.swift
@@ -0,0 +1,31 @@
+//
+//  InfoViewController.swift
+//  ShareUp
+//
+//  Created by 이가영 on 2021/06/01.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "정보"
+        navigationBackCustom()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
