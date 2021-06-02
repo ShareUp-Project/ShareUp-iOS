b33d8ded61ce73e53da526256c9400682157cc2f
Ga-yo <39812@naver.com>
2021-06-02 14:48:19 +0900

    [Fix] 배지 오타 수정

--- ShareUp/ShareUp/Source/Util/Filter.swift
+++ ShareUp/ShareUp/Source/Util/Filter.swift
@@ -17,7 +17,7 @@ struct ShareUpFilter {
         case "유리" : return "glass"
         case "플라스틱" : return "plastic"
         case "종이" : return "paper"
-        case "스티로폼" : return "styroform"
+        case "스티로폼" : return "styrofoam"
         case "캔" : return "can"
         case "비닐" : return "vinyl"
         case "의류" : return "clothing"
@@ -33,7 +33,7 @@ struct ShareUpFilter {
         case 3 : return "can"
         case 4 : return "plastic"
         case 5 : return "glass"
-        case 6 : return "styroform"
+        case 6 : return "styrofoam"
         case 7 : return "clothing"
         case 8 : return "etc"
         default: return ""
@@ -45,11 +45,11 @@ struct ShareUpFilter {
         case 0 : return "default"
         case 1: return "first"
         case 2 : return "paper"
-        case 3 : return "vinyl"
+        case 3 : return "plastic"
         case 4 : return "glass"
-        case 5 : return "plastic"
-        case 6 : return "can"
-        case 7 : return "styrofoam"
+        case 5 : return "styrofoam"
+        case 6 : return "vinyl"
+        case 7 : return "can"
         case 8 : return "clothing"
         default: return ""
         }
@@ -60,11 +60,11 @@ struct ShareUpFilter {
         case "default" : return 0
         case "first": return 1
         case "paper" : return 2
-        case "vinyl" : return 3
+        case "plastic" : return 3
         case "glass" : return 4
-        case "plastic" : return 5
-        case "can" : return 6
-        case "styrofoam" : return 7
+        case "styrofoam" : return 5
+        case "vinyl" : return 6
+        case "can" : return 7
         case "clothing": return 8
         default: return 0
         }
